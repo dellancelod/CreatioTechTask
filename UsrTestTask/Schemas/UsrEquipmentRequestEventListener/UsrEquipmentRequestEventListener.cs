2f72a5b8e3395a870a0eabec3b456e8e64635a6c
Danil Svetaylo <danil.svetaylo@gmail.com>
2026-09-19 15:10:40 +0300

    feat: add equipment request validation and total calculation

---
+++ UsrTestTask/Schemas/UsrEquipmentRequestEventListener/UsrEquipmentRequestEventListener.cs
@@ -0,0 +1,46 @@
+ namespace Terrasoft.Configuration
+{
+	using System;
+	using Terrasoft.Core.Entities;
+	using Terrasoft.Core.Entities.Events;
+
+	
+	[EntityEventListener(SchemaName = "UsrEquipmnetRequest")]
+	public class UsrEquipmentRequestEventListener : BaseEntityEventListener {
+		public override void OnSaving(
+			object sender,
+			EntityBeforeEventArgs e
+		)
+		{
+			base.OnSaving(sender, e);
+			
+			var entity = (Entity)sender;
+
+			int quantity = entity.GetTypedColumnValue<int>("UsrQuantity");
+
+			decimal unitPrice = entity.GetTypedColumnValue<decimal>("UsrUnitPrice");
+
+			DateTime executionDate = entity.GetTypedColumnValue<DateTime>("UsrExecutionDate");
+
+			if (quantity <= 0){
+				throw new InvalidOperationException(
+					"Кількість має бути більше нуля!"
+				);
+			}
+			if(executionDate != default(DateTime) &&
+				executionDate.Date < DateTime.Today){
+				throw new InvalidOperationException(
+					"Дата виконання не може бути в минулому!"
+				);
+			}
+			decimal totalAmount = quantity * unitPrice;
+			
+			entity.SetColumnValue(
+				"UsrToalAmount",
+				totalAmount
+			);
+			
+		}
+		
+	}
+}
